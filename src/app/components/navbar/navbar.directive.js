a7841c1e91504c40187ef53705483f8ec9b275eb
Blanca Azucena López <blanca.lopez@data4.mx>
2016-10-14 09:58:51 -0500

    Example using comparation to show different paragraphs

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -25,6 +25,8 @@
       var vm = this;
       var pageWidth = $window.screen.availWidth;
       vm.mobile = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || pageWidth < 620 && pageWidth > 0) ? true : false;  
+      vm.data = $languageService.data;
+      
     }
   }
 
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -11,7 +11,7 @@
 
     var pageWidth = $window.screen.availWidth;
     vm.mobile = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || pageWidth < 620 && pageWidth > 0) ? true : false;
-    
-    
+    vm.data = $languageService.data;
+    //vm.data.language cambia a 'english' o 'spanish' segun se seleccione en el navbar
   }
 })();
