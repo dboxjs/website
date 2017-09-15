c75cc1c1ab7d3a572a432195d84ab06872efcf02
nellalela <nella.rg25@gmail.com>
2017-09-15 13:36:17 -0500

    showcase-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowcaseComponent
   ],
   imports: [
     BrowserModule,
