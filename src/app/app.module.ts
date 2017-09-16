f0d998309a17d789aa44d1a9bcf3fad0a86e451c
Paola <Paola@MacBook-Pro-de-Paola.local>
2017-09-15 20:13:53 -0500

    add: home with pug and subheader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SubheaderComponent } from './subheader/subheader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SubheaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/subheader/subheader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subheader',
+  templateUrl: './subheader.component.html',
+  styleUrls: ['./subheader.component.css']
+})
+export class SubheaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
