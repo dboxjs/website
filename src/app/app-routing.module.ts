5b5dc36acdf0a964bd336278fe70a7a106764101
Berta Díaz <berta@MacBook-Pro-de-Berta.local>
2017-09-15 22:19:48 -0500

    Add: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ChartComponent } from './chart/chart.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: HomeComponent
+
+  },
+  {
+    path: 'dbox/:chart',
+    component: ChartComponent
   }
 ];
 
