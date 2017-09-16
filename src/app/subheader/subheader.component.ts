3ca88642536dde8061165a4ff408147fe304718c
Paola <Paola@MacBook-Pro-de-Paola.local>
2017-09-15 20:55:06 -0500

    add:stylus

--- src/app/subheader/subheader.component.ts
+++ src/app/subheader/subheader.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-subheader',
   templateUrl: './subheader.component.html',
-  styleUrls: ['./subheader.component.css']
+  styleUrls: ['./subheader.component.styl']
 })
 export class SubheaderComponent implements OnInit {
 
