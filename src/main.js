dd3401bd8422cedbb8e6f85dde122e1b064fe9c3
Jorge Andrade <jorandradefig@gmail.com>
2018-03-14 08:47:51 -0600

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  Vuetify,
   render: h => h(App)
 }).$mount("#app");
