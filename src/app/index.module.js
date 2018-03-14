4b71df08d5619814baa6dbe24dfa6e240e55683c
Jorge Andrade <jorandradefig@gmail.com>
2018-03-14 08:24:50 -0600

    Remove angular project

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('dboxLibrary', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ui.bootstrap', 'toastr', 'dbox', 'pascalprecht.translate']);
-
-})();
